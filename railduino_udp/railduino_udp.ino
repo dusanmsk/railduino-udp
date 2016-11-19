4e2375cd39bbdc29b3c0e90c85313c2724c22c5b
Dusan Zatkovsky <dusanmsk@gmail.com>
2016-11-19 18:23:23 +0100

    added ping todo - split addressing and commands

--- railduino_udp/railduino_udp.ino
+++ railduino_udp/railduino_udp.ino
@@ -1,6 +1,6 @@
 /*
     Copyright (C) 2016  Dusan Zatkovsky
-    
+
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
@@ -14,7 +14,23 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-    
+
+ */
+
+
+ /*
+    Wire syntax:
+
+    signals:
+      1wire sensor packet:            rail1 1wire 2864fc3008082 25.44
+      digital input connected:        rail1 i1 1
+      digital input disconnected:     rail1 i1 0
+
+    commands:
+      relay on command:               rail1 r12 on
+      relay off command:              rail1 r5 off
+      digital inputs configuration:   rail1 cfg repeat=4,5,8,12 period=5000
+
  */
 
 #define oneWireSleepRatio 2
@@ -29,9 +45,9 @@
 #include <EthernetUdp.h>
 
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0 }; // last byte will be derived from board address
-unsigned int listenPort = 44444; 
-unsigned int sendPort = 55554; 
-unsigned int loxonePort = 55555; 
+unsigned int listenPort = 44444;
+unsigned int sendPort = 55554;
+unsigned int loxonePort = 55555;
 IPAddress listenIpAddress;
 IPAddress sendIpAddress(255, 255, 255, 255);
 
@@ -54,11 +70,15 @@ int relayPins[] = {39,41,43,45,47,49,23,25,27,29,31,33};
 int inputPins[] = {36,34,48,46,69,68,67,66,44,42,40,38,6,5,3,2,14,15,16,17,24,26,28,30};
 int inputStatus[numOfInputs];
 
+int statusLedPin = 32;
+
 int boardAddress = 0;
 String boardAddressStr;
 
 String relayOnCommands[numOfRelays];
 String relayOffCommands[numOfRelays];
+String configurationCommandTemplate;
+String pingCommandTemplate;
 
 class Timer {
   private:
@@ -74,8 +94,8 @@ class Timer {
  */
 boolean Timer::isOver() {
   if( millis() - timestampLastHitMs < sleepTimeMs ) {
-    return false; 
-  }    
+    return false;
+  }
   timestampLastHitMs = millis();
   return true;
 }
@@ -86,34 +106,50 @@ void Timer::sleep(unsigned long sleepTimeMs) {
 }
 
 
+Timer statusLedTimer;
+void blinkStatusLed() {
+  digitalWrite(statusLedPin, 255);
+  statusLedTimer.sleep(50);
+}
+
+void turnOffStatusLed() {
+  if(statusLedTimer.isOver()) {
+    digitalWrite(statusLedPin, 0);
+  }
+}
+
+
 void setup() {
+
+  blinkStatusLed();
+
   Serial.begin(9600);
 
  /* COMMON settings */
   for(int i = 0; i < numOfRelays; i++) {
-    pinMode(relayPins[i], OUTPUT);  
+    pinMode(relayPins[i], OUTPUT);
   }
-  pinMode(32, OUTPUT);  // LED
-  
+  pinMode(statusLedPin, OUTPUT);  // LED
+
   /* digital inputs settings */
   for(int i = 0; i < numOfInputs; i++) {
-    pinMode(inputPins[i], INPUT_PULLUP);   
+    pinMode(inputPins[i], INPUT_PULLUP);
     inputStatus[i] = 0;
   }
-  
+
   /* DIP SWITCH settings */
-  pinMode(57, INPUT);  
+  pinMode(57, INPUT);
   pinMode(56, INPUT);
   pinMode(55, INPUT);
   pinMode(54, INPUT);
-  
-  /* ANALOG INPUTS */  
+
+  /* ANALOG INPUTS */
   pinMode(A4, INPUT);
   pinMode(A5, INPUT);
   pinMode(A8, INPUT);
-  
+
   /* PWM OUTPUTS */
-  pinMode(7, OUTPUT);  
+  pinMode(7, OUTPUT);
   pinMode(11, OUTPUT);
   pinMode(12, OUTPUT);
   pinMode(13, OUTPUT);
@@ -123,21 +159,23 @@ void setup() {
   if (!digitalRead(55)) {boardAddress |= 2;}
   if (!digitalRead(56)) {boardAddress |= 4;}
   if (!digitalRead(57)) {boardAddress |= 8;}
-  
+
   boardAddressStr = String(boardAddress);
 
-  // generate relay on/off command templates (speedup matching when receiving udp packet)
+  // generate command templates (speedup matching when receiving udp packet)
   for(int i = 0; i < numOfRelays; i++) {
      relayOnCommands[i] = "rail" + boardAddressStr + " r" + String(i + 1, DEC) + " on";
      relayOffCommands[i] = "rail" + boardAddressStr + " r" + String(i + 1, DEC) + " off";
   }
+  configurationCommandTemplate = "rail" + boardAddressStr + " cfg";
+  pingCommandTemplate = "rail" + boardAddressStr + " ping";
 
    // initialize ethernet
    mac[5] = (0xED + boardAddress);
    listenIpAddress = IPAddress(192, 168, 150, 150 + boardAddress);
    Ethernet.begin(mac, listenIpAddress);
-   udpRecv.begin(listenPort);    
-   udpSend.begin(sendPort);    
+   udpRecv.begin(listenPort);
+   udpSend.begin(sendPort);
 
   dbg("Railduino address: ");
   dbg(boardAddressStr);
@@ -146,7 +184,7 @@ void setup() {
   for(int i = 0; i < numOfRelays; i++) {
     setRelay(i, 0);
   }
-    
+
 }
 
 void loop() {
@@ -154,7 +192,7 @@ void loop() {
   // read inputs and send updates
   // TODO zasekava sa (!!)
   readInputs();
-  
+
   // process relay commands
   processCommands();
 
@@ -162,6 +200,8 @@ void loop() {
   processOnewire();
 
   processRepeatedInputs();
+
+  turnOffStatusLed();
 
   delay(10);
 
@@ -172,7 +212,7 @@ void processRepeatedInputs() {
   if(!repeatedInputsTimer.isOver()) {
     return;
   }
-  
+
   dbg("TODO repeated inputs");
 
   repeatedInputsTimer.sleep(repeatedInputsSleepRatio * 1000);
@@ -199,9 +239,9 @@ void processOnewire() {
   if(!oneWireTimer.isOver()) {
     return;
   }
-  
+
   dbg("processing onewire")
-  
+
   switch(oneWireConversationState) {
     case SEARCH:
       if ( !ds.search(oneWireAddr) )  {
@@ -223,11 +263,11 @@ void processOnewire() {
     case READ:
       byte type_s;
       byte present = ds.reset();
-      ds.select(oneWireAddr);    
+      ds.select(oneWireAddr);
       ds.write(0xBE);                     // Read Scratchpad
       for ( int i = 0; i < 9; i++) {          // we need 9 bytes
           oneWireData[i] = ds.read();
-      }      
+      }
       if ( OneWire::crc8(oneWireData, 8) == oneWireData[8] ) {
         switch (oneWireAddr[0]) {
           case 0x10:  // DS18S20/DS1820
@@ -242,8 +282,8 @@ void processOnewire() {
           default:
             dbg("Unknown 1wire device");
             return;
-        } 
-      
+        }
+
         int16_t raw = (oneWireData[1] << 8) | oneWireData[0];
         if (type_s) {
           raw = raw << 3; // 9 bit resolution default
@@ -266,7 +306,7 @@ void processOnewire() {
       oneWireTimer.sleep(1000);
       return;
   }
-  
+
 }
 
 
@@ -314,6 +354,7 @@ void setRelay(int relay, int value) {
 String receivePacket() {
   int packetSize = udpRecv.parsePacket();
   if (packetSize) {
+    blinkStatusLed();
     memset(inputPacketBuffer, 0, sizeof(inputPacketBuffer));
     udpRecv.read(inputPacketBuffer, inputPacketBufferSize);
     String cmd = String(inputPacketBuffer);
@@ -323,17 +364,29 @@ String receivePacket() {
   return "";
 }
 
+void processConfigurationCommand(String cmd) {
+    String tmp = cmd.substring(configurationCommandTemplate.length());
+}
+
+void processPingCommand(String cmd) {
+    blinkStatusLed();
+}
+
 void processCommands() {
     String cmd = receivePacket();
     if(cmd != "") {
-      for(int i = 0; i < numOfRelays; i++) {
-        if(cmd == relayOnCommands[i]) {
-          setRelay(i, 1);
-        } else if(cmd == relayOffCommands[i]) {
-          setRelay(i, 0);
+      if(cmd.startsWith(configurationCommandTemplate)) {
+        processConfigurationCommand(cmd);
+      } else if(cmd.startsWith(pingCommandTemplate)) {
+        processPingCommand(cmd);
+      } else {
+        for(int i = 0; i < numOfRelays; i++) {
+          if(cmd == relayOnCommands[i]) {
+            setRelay(i, 1);
+          } else if(cmd == relayOffCommands[i]) {
+            setRelay(i, 0);
+          }
         }
       }
     }
   }
-
-
